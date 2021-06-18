a520569d7d3821fcb5c5f26a343da5601265c4f7
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-06-18 11:13:04 +0200

    complete integration test incl. request

--- cypress/integration/requests/request.js
+++ cypress/integration/requests/request.js
@@ -42,88 +42,88 @@
 
 // Deterministic test with set-up and teardown!
 
-// describe("JWT request", () => {
-//   before(() => {
-//     // Cypress.Commands
-//     cy.signUp();
-//   });
-//   after(() => {
-//     cy.deleteUsers();
-//     // cy.log("teardown");
-//     // cy.request("GET", "http://localhost:8080/users").then((response) => {
-//     //   cy.log("Response", response.body.forEach);
-//     //   response.body.forEach((user) => {
-//     //     // cy.log("User", user.email);
-//     //     cy.request({
-//     //       method: "DELETE",
-//     //       url: `http://localhost:8080/users/${user.id}`,
-//     //     });
-//     //   });
-//     // });
-//   });
-//   it("SECURED API request", () => {
-//     cy.get("@token").then((token) => {
-//       cy.request({
-//         method: "GET",
-//         url: "http://localhost:8080/courses",
-//         auth: {
-//           bearer: token,
-//         },
-//       });
-//     });
-//   });
-// });
+describe("JWT request", () => {
+  before(() => {
+    // Cypress.Commands
+    cy.signUp();
+  });
+  after(() => {
+    cy.deleteUsers();
+    // cy.log("teardown");
+    // cy.request("GET", "http://localhost:8080/users").then((response) => {
+    //   cy.log("Response", response.body.forEach);
+    //   response.body.forEach((user) => {
+    //     // cy.log("User", user.email);
+    //     cy.request({
+    //       method: "DELETE",
+    //       url: `http://localhost:8080/users/${user.id}`,
+    //     });
+    //   });
+    // });
+  });
+  it("SECURED API request", () => {
+    cy.get("@token").then((token) => {
+      cy.request({
+        method: "GET",
+        url: "http://localhost:8080/courses",
+        auth: {
+          bearer: token,
+        },
+      });
+    });
+  });
+});
 
-// describe("request command suite", () => {
-//   before(() => {
-//     // cy.log("set up");
-//     cy.request("POST", "http://localhost:8080/todos", {
-//       name: "TEST",
-//       isComplete: false,
-//     }).then((response) => {
-//       expect(response.status).to.eq(201);
-//     });
-//   });
+describe("request command suite", () => {
+  before(() => {
+    // cy.log("set up");
+    cy.request("POST", "http://localhost:8080/todos", {
+      name: "TEST",
+      isComplete: false,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+    });
+  });
 
-//   after(() => {
-//     // cy.log("kill the server!");
-//     cy.request("GET", "http://localhost:8080/todos").then((response) => {
-//       // cy.log("Response", response.body.forEach);
-//       response.body.forEach((todo) => {
-//         // cy.log("Todo", todo.id);
-//         cy.request({
-//           method: "DELETE",
-//           url: `http://localhost:8080/todos/${todo.id}`,
-//         });
-//       });
-//     });
-//   });
-//   it("Get request", () => {
-//     cy.visit("http://localhost:3000");
-//     //cy.log("Hello World");
-//     cy.request({
-//       method: "GET",
-//       url: "http://localhost:8080/todos",
-//       qs: { id: 1 },
-//       //with qs you add the parameters that you want to send along with the request
-//     }).then((response) => {
-//       cy.log(response.statusText);
-//       expect(response.body[0].isComplete).to.be.false;
-//     });
-//   });
-//   it("POST request", () => {
-//     cy.request("POST", "http://localhost:8080/todos", {
-//       name: "TEST",
-//       isComplete: false,
-//     }).then((response) => {
-//       expect(response.status).to.eq(201);
-//     });
-//   });
-//   it("PUT request", () => {
-//     cy.request("PUT", "http://localhost:8080/todos/1", {
-//       name: "Study Cypress",
-//       id: 1,
-//       isComplete: true,
-//     });
-//   });
-// });
+  after(() => {
+    // cy.log("kill the server!");
+    cy.request("GET", "http://localhost:8080/todos").then((response) => {
+      // cy.log("Response", response.body.forEach);
+      response.body.forEach((todo) => {
+        // cy.log("Todo", todo.id);
+        cy.request({
+          method: "DELETE",
+          url: `http://localhost:8080/todos/${todo.id}`,
+        });
+      });
+    });
+  });
+  it("Get request", () => {
+    cy.visit("http://localhost:3000");
+    //cy.log("Hello World");
+    cy.request({
+      method: "GET",
+      url: "http://localhost:8080/todos",
+      qs: { id: 1 },
+      //with qs you add the parameters that you want to send along with the request
+    }).then((response) => {
+      cy.log(response.statusText);
+      expect(response.body[0].isComplete).to.be.false;
+    });
+  });
+  it("POST request", () => {
+    cy.request("POST", "http://localhost:8080/todos", {
+      name: "TEST",
+      isComplete: false,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+    });
+  });
+  it("PUT request", () => {
+    cy.request("PUT", "http://localhost:8080/todos/1", {
+      name: "Study Cypress",
+      id: 1,
+      isComplete: true,
+    });
+  });
+});
